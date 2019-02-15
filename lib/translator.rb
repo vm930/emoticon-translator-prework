34fe5ca2bd52d1ba6ce552f8c6a07af0c1a4ca70
abstract-animation-9914 <vm930@hotmail.com>
2019-02-15 03:34:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,16 +15,16 @@ end
   library
 end 
 
-require 'yaml'
-def load_library(file_path)
-  library = {"get_meaning" => {}, "get_emoticon" => {} }
-  YAML.load_file(file_path).each do |meaning, array|
-    english, japanese = array
-    library["get_emoticon"][english] = japanese
-    library["get_meaning"][japanese] = meaning
-  end
-  library
-end
+# require 'yaml'
+# def load_library(file_path)
+#   library = {"get_meaning" => {}, "get_emoticon" => {} }
+#   YAML.load_file(file_path).each do |meaning, array|
+#     english, japanese = array
+#     library["get_emoticon"][english] = japanese
+#     library["get_meaning"][japanese] = meaning
+#   end
+#   library
+# end
 
 def get_japanese_emoticon(file_path,emoticon)
   library = load_library(file_path)
