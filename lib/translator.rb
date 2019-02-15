4184de9b18fb1b76811b13526cf3d90872885873
abstract-animation-9914 <vm930@hotmail.com>
2019-02-15 03:07:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,31 +1,49 @@
 require 'yaml'
 
+# angel:
+#   - "O:)"      
+#   - "☜(⌒▽⌒)☞
+
 def load_library(file_path)
-  library = {"get_meaning" => {}, "get_emoticon" => {} }
-  YAML.load_file(file_path).each do |meaning, array|
-    english, japanese = array
-    library["get_emoticon"][english] = japanese
-    library["get_meaning"][japanese] = meaning
-  end
-  library
-end
+  library = {"get_meaning" =>{}, "get_emoticon" =>{}}
+  
+  YAML.load_file(file_path).each do |meaning, array| #angel => {["",""]}
+   english, japanese = array
+   library["get_emoticon"][english] = japanese
+   library["get_meaning"][japense] = meaning
+ end 
+  library 
+end 
 
-def get_japanese_emoticon(file_path, emoticon)
-  library = load_library(file_path)
-  result = library["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
 
-def get_english_meaning(file_path, emoticon)
-  library = load_library(file_path)
-  result = library["get_meaning"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+# require 'yaml'
+
+# def load_library(file_path)
+#   library = {"get_meaning" => {}, "get_emoticon" => {} }
+#   YAML.load_file(file_path).each do |meaning, array|
+#     english, japanese = array
+#     library["get_emoticon"][english] = japanese
+#     library["get_meaning"][japanese] = meaning
+#   end
+#   library
+# end
+
+# def get_japanese_emoticon(file_path, emoticon)
+#   library = load_library(file_path)
+#   result = library["get_emoticon"][emoticon]
+#   if result
+#     result
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
+
+# def get_english_meaning(file_path, emoticon)
+#   library = load_library(file_path)
+#   result = library["get_meaning"][emoticon]
+#   if result
+#     result
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
