1770c6e6f826f61a3e6a9fdf7112ba129b842428
abstract-animation-9914 <vm930@hotmail.com>
2019-02-15 03:33:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,15 +38,15 @@ end
 #   end 
 # end 
 
-def get_japanese_emoticon(file_path, emoticon)
-  library = load_library(file_path)
-  result = library["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+# def get_japanese_emoticon(file_path, emoticon)
+#   library = load_library(file_path)
+#   result = library["get_emoticon"][emoticon]
+#   if result
+#     result
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
 
 # def get_english_meaning(file_path, emoticon)
 #   library = load_library(file_path)
